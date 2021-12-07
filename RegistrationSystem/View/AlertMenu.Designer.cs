901479e7e5e7362d3c968602fd33afe15c8b5211
TheOctan <kanatush.tim@gmail.com>
2021-12-07 21:01:17 +0300

    Alert menu

---
+++ RegistrationSystem/View/AlertMenu.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Catalog
+{
+    partial class AlertMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.Cancel = new System.Windows.Forms.Button();
+			this.Message = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// Cancel
+			// 
+			this.Cancel.Location = new System.Drawing.Point(82, 76);
+			this.Cancel.Name = "Cancel";
+			this.Cancel.Size = new System.Drawing.Size(75, 23);
+			this.Cancel.TabIndex = 0;
+			this.Cancel.Text = "Ok";
+			this.Cancel.UseVisualStyleBackColor = true;
+			this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+			// 
+			// Message
+			// 
+			this.Message.AutoSize = true;
+			this.Message.Location = new System.Drawing.Point(12, 9);
+			this.Message.Name = "Message";
+			this.Message.Size = new System.Drawing.Size(35, 13);
+			this.Message.TabIndex = 1;
+			this.Message.Text = "label1";
+			// 
+			// AlertMenu
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ClientSize = new System.Drawing.Size(234, 111);
+			this.Controls.Add(this.Message);
+			this.Controls.Add(this.Cancel);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "AlertMenu";
+			this.Text = "Registry system";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Label Message;
+    }
+}
